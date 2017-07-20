03dcc5c9b80d88061de5fc617920acac501fccab
Kiran Tomlinson <kirantomlinson@gmail.com>
2017-07-20 00:13:15 -0500

    Minor changes to GifSequenceWriter

--- library/GifSequenceWriter.java
+++ library/GifSequenceWriter.java
@@ -10,12 +10,19 @@ package mandelbrot.library;
 // http://creativecommons.org/licenses/by/3.0/ or send a letter to Creative
 // Commons, 171 Second Street, Suite 300, San Francisco, California, 94105, USA.
 
+/*
+ Modified by Kiran Tomlinson 2017-7-20
+ */
+
 
 import javax.imageio.*;
-import javax.imageio.metadata.*;
-import javax.imageio.stream.*;
-import java.awt.image.*;
-import java.io.*;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.imageio.stream.FileImageOutputStream;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Iterator;
 
 public class GifSequenceWriter {
@@ -26,7 +33,6 @@ public class GifSequenceWriter {
     /**
      * Creates a new GifSequenceWriter
      *
-     * @param outputStream the ImageOutputStream to be written to
      * @param imageType one of the imageTypes specified in BufferedImage
      * @param timeBetweenFramesMS the time between frames in miliseconds
      * @param loopContinuously wether the gif should loop repeatedly
@@ -34,48 +40,33 @@ public class GifSequenceWriter {
      *
      * @author Elliot Kroo (elliot[at]kroo[dot]net)
      */
-    public GifSequenceWriter(
-            ImageOutputStream outputStream,
-            int imageType,
-            int timeBetweenFramesMS,
-            boolean loopContinuously) throws IIOException, IOException {
-        // my method to create a writer
+    public GifSequenceWriter(String outputFile, int imageType, int timeBetweenFramesMS, boolean loopContinuously) throws IOException {
+
+        ImageOutputStream outputStream = new FileImageOutputStream(new File(outputFile));
+
         gifWriter = getWriter();
         imageWriteParam = gifWriter.getDefaultWriteParam();
-        ImageTypeSpecifier imageTypeSpecifier =
-                ImageTypeSpecifier.createFromBufferedImageType(imageType);
+        ImageTypeSpecifier imageTypeSpecifier = ImageTypeSpecifier.createFromBufferedImageType(imageType);
 
-        imageMetaData =
-                gifWriter.getDefaultImageMetadata(imageTypeSpecifier,
-                        imageWriteParam);
+        imageMetaData = gifWriter.getDefaultImageMetadata(imageTypeSpecifier, imageWriteParam);
 
         String metaFormatName = imageMetaData.getNativeMetadataFormatName();
 
         IIOMetadataNode root = (IIOMetadataNode)
                 imageMetaData.getAsTree(metaFormatName);
 
-        IIOMetadataNode graphicsControlExtensionNode = getNode(
-                root,
-                "GraphicControlExtension");
+        IIOMetadataNode graphicsControlExtensionNode = getNode(root,"GraphicControlExtension");
 
         graphicsControlExtensionNode.setAttribute("disposalMethod", "none");
         graphicsControlExtensionNode.setAttribute("userInputFlag", "FALSE");
-        graphicsControlExtensionNode.setAttribute(
-                "transparentColorFlag",
-                "FALSE");
-        graphicsControlExtensionNode.setAttribute(
-                "delayTime",
-                Integer.toString(timeBetweenFramesMS / 10));
-        graphicsControlExtensionNode.setAttribute(
-                "transparentColorIndex",
-                "0");
+        graphicsControlExtensionNode.setAttribute("transparentColorFlag", "FALSE");
+        graphicsControlExtensionNode.setAttribute("delayTime", Integer.toString(timeBetweenFramesMS / 10));
+        graphicsControlExtensionNode.setAttribute("transparentColorIndex", "0");
 
-        IIOMetadataNode commentsNode = getNode(root, "CommentExtensions");
+        IIOMetadataNode commentsNode = getNode(root,"CommentExtensions");
         commentsNode.setAttribute("CommentExtension", "Created by MAH");
 
-        IIOMetadataNode appEntensionsNode = getNode(
-                root,
-                "ApplicationExtensions");
+        IIOMetadataNode appEntensionsNode = getNode(root,"ApplicationExtensions");
 
         IIOMetadataNode child = new IIOMetadataNode("ApplicationExtension");
 
@@ -84,8 +75,7 @@ public class GifSequenceWriter {
 
         int loop = loopContinuously ? 0 : 1;
 
-        child.setUserObject(new byte[]{ 0x1, (byte) (loop & 0xFF), (byte)
-                ((loop >> 8) & 0xFF)});
+        child.setUserObject(new byte[]{ 0x1, (byte) (loop & 0xFF), (byte) ((loop >> 8) & 0xFF)});
         appEntensionsNode.appendChild(child);
 
         imageMetaData.setFromTree(metaFormatName, root);
@@ -96,12 +86,7 @@ public class GifSequenceWriter {
     }
 
     public void writeToSequence(RenderedImage img) throws IOException {
-        gifWriter.writeToSequence(
-                new IIOImage(
-                        img,
-                        null,
-                        imageMetaData),
-                imageWriteParam);
+        gifWriter.writeToSequence(new IIOImage(img, null, imageMetaData), imageWriteParam);
     }
 
     /**
@@ -137,13 +122,10 @@ public class GifSequenceWriter {
      *
      * @return the child node, if found or a new node created with the given name.
      */
-    private static IIOMetadataNode getNode(
-            IIOMetadataNode rootNode,
-            String nodeName) {
+    private static IIOMetadataNode getNode(IIOMetadataNode rootNode, String nodeName) {
         int nNodes = rootNode.getLength();
         for (int i = 0; i < nNodes; i++) {
-            if (rootNode.item(i).getNodeName().compareToIgnoreCase(nodeName)
-                    == 0) {
+            if (rootNode.item(i).getNodeName().compareToIgnoreCase(nodeName) == 0) {
                 return((IIOMetadataNode) rootNode.item(i));
             }
         }
@@ -152,41 +134,26 @@ public class GifSequenceWriter {
         return(node);
     }
 
-    /**
-     public GifSequenceWriter(
-     BufferedOutputStream outputStream,
-     int imageType,
-     int timeBetweenFramesMS,
-     boolean loopContinuously) {
 
-     */
-
-    public static void main(String[] args) throws Exception {
-        if (args.length > 1) {
-            // grab the output image type from the first image in the sequence
-            BufferedImage firstImage = ImageIO.read(new File(args[0]));
-
-            // create a new BufferedOutputStream with the last argument
-            ImageOutputStream output =
-                    new FileImageOutputStream(new File(args[args.length - 1]));
-
-            // create a gif sequence with the type of the first image, 1 second
-            // between frames, which loops continuously
-            GifSequenceWriter writer =
-                    new GifSequenceWriter(output, firstImage.getType(), 1, false);
-
-            // write out the first image to our sequence...
-            writer.writeToSequence(firstImage);
-            for(int i=1; i<args.length-1; i++) {
-                BufferedImage nextImage = ImageIO.read(new File(args[i]));
-                writer.writeToSequence(nextImage);
-            }
-
-            writer.close();
-            output.close();
-        } else {
-            System.out.println(
-                    "Usage: java GifSequenceWriter [list of gif files] [output file]");
-        }
-    }
+//    public static void main(String[] args) throws Exception {
+//        if (args.length > 1) {
+//
+//            // create a gif sequence with the type of the first image, 1 second
+//            // between frames, which loops continuously
+//            GifSequenceWriter writer = new GifSequenceWriter("out.gif", firstImage.getType(), 1, false);
+//
+//            // write out the first image to our sequence...
+//            writer.writeToSequence(firstImage);
+//            for(int i=1; i<args.length-1; i++) {
+//                BufferedImage nextImage = ImageIO.read(new File(args[i]));
+//                writer.writeToSequence(nextImage);
+//            }
+//
+//            writer.close();
+//            output.close();
+//        } else {
+//            System.out.println(
+//                    "Usage: java GifSequenceWriter [list of gif files] [output file]");
+//        }
+//    }
 }
